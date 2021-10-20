450191ea8f99d0461d784c31d83952df1242ff7e
Vitalii Kopiton <dd150285kvn@gmail.com>
2021-10-21 00:52:43 +0300

    No while loop

--- src/com/pb/kopiton/hw3/Bingo.java
+++ src/com/pb/kopiton/hw3/Bingo.java
@@ -1,3 +1,5 @@
+// block diagram - https://docs.google.com/drawings/d/1MlZn5V7VkfcvARPDDRArOjDhSw6Pv2_4tn2NYtWfMTU/edit
+
 package com.pb.kopiton.hw3;
 
 import java.util.Random;
@@ -6,17 +8,43 @@ import java.util.Scanner;
 public class Bingo {
     public static void main(String[] args) {
 
-        Scanner fin = new Scanner(System.in);
-        System.out.print("Хотите продолжить? Если да, укажите  Y  ");
-        String fin1 = fin.next();
+        Scanner end = new Scanner(System.in);
+        System.out.println("Start Game");
+        System.out.println("Хотите продолжить? Если да, укажите  Y : ");
+        String go = end.next();
 
-        if (fin1.equals ("Y")) {
+        if (go.equals ("Y") || go.equals("y")) {
+
             int random = (int) (Math.random()*100);
 
             Scanner a = new Scanner(System.in);
-            System.out.print("Угадай число от 0 до 100: ");
-            int b = a.nextInt();
+            System.out.println("Угадай число от 0 до 100: ");
+            int number = a.nextInt();
 
+            if (number == random) {
+                System.out.print ("Вы выйграли :)");
+            }
+
+            else {
+                System.out.println("Вы не угадали :(");
+                System.out.println("Загаданное число " + random);
+                if (random>number) {
+                    System.out.print("Загаданное число > чем " + number);
+                }
+                if (random<number) {
+                    System.out.println("Загаданое число < чем " + number);
+                }
+                // с
+            }
+
+
+
+        }
+
+
+
+        else {
+            System.out.println("Game Over");
         }
 
     }
