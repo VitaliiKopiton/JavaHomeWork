1e9c1f2b0ae5d15cdb9ab64a50c1156b09eb2485
Vitalii Kopiton <dd150285kvn@gmail.com>
2021-10-21 20:18:53 +0300

    tested

--- src/com/pb/kopiton/hw3/Bingo.java
+++ src/com/pb/kopiton/hw3/Bingo.java
@@ -8,32 +8,30 @@ import java.util.Scanner;
 public class Bingo {
     public static void main(String[] args) {
 
+        System.out.println("Start Game");
+
         int random = (int) (Math.random()*100);
+        int number;       // если просто создать переменную, то ошибка, почему ?
         int counter = 0;
-        int number;
 
         do {
             counter ++;
-            //int number1;
 
             Scanner end = new Scanner(System.in);
-            System.out.println("Start Game");
             System.out.println("Хотите продолжить? Если да, укажите  Y : ");
             String go = end.next();
 
             if (go.equals ("Y") || go.equals("y")) {
 
-                 //int number1;
-
-                Scanner a = new Scanner(System.in);
-                System.out.println("Попытка " + counter + ". Угадай число от 0 до 100: ");
-                number = a.nextInt();
+                Scanner num = new Scanner(System.in);
+                System.out.println("Попытка " + counter + " Угадай число от 0 до 100: ");
+                number = num.nextInt();
 
                 if (number == random) {
-                    System.out.print("Вы угадали с " + counter + " попытки :)");
+                    System.out.println("Вы угадали с " + counter + " попытки :)");
                 } else {
                     System.out.println("Вы не угадали :(");
-                    System.out.println("Загаданное число " + random);
+                    //System.out.println("Загаданное число " + random);
                     if (random > number) {
                         System.out.println("Загаданное число > чем " + number);
                     }
@@ -43,12 +41,15 @@ public class Bingo {
 
                 }
 
-            }  //if
+            }
             else {
-                System.out.println("Game Over");
+                break;
             }
 
-        }  //do
-        while (number != random);
+        }
+        while (random != number);
+        System.out.println ("Game Over");
+
     }
+
 }
