57e0b6700f793997eb1dbbf6b10c5b50a89e0336
Vitalii Kopiton <dd150285kvn@gmail.com>
2021-11-15 23:55:35 +0200

    hw7

---
+++ src/com/pb/kopiton/hw7/Atelier.java
@@ -0,0 +1,7 @@
+package com.pb.kopiton.hw7;
+
+public class Atelier {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/pb/kopiton/hw7/Size.java
@@ -0,0 +1,7 @@
+package com.pb.kopiton.hw7;
+
+public enum Size {
+    XXS;
+//    XS; S;
+//    M; L;
+}
