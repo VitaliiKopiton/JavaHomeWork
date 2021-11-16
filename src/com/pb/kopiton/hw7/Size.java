86d3d456675d16eb995e9a5564b56ffeb664b0bb
Vitalii Kopiton <dd150285kvn@gmail.com>
2021-11-16 10:09:25 +0200

    hw7

--- src/com/pb/kopiton/hw7/Size.java
+++ src/com/pb/kopiton/hw7/Size.java
@@ -8,14 +8,40 @@ public enum Size {
       L ("l");
 
     public Enum (String description, String euroSize ) {
+        description = description;
+        euroSize = euroSize
 
     }
 
     public getDescription() {
+            if (euroSize equals ('xxs') || euroSize equals ('XXS')) {
+                return 'Детский размер';
+                elso {}
+
+            }
 
     }
 
-    public getEuroSize() {
-
+    public int getEuroSize() {
+        switch (euroSize) {
+            case 'xxs':
+                return 32;
+                break;
+            case 'xs':
+                return 34;
+                break;
+            case 's':
+                return 36;
+                break;
+            case 'm':
+                return 38;
+                break;
+            case 'l':
+                return 40;
+                break;
+        }
+        default:
+        System.out.println(euroSize + " - размер указан не верно");
     }
+
 }
