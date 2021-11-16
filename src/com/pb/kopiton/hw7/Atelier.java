b5a579d2499a9a3dd72f8f8987d87031cecc8453
Vitalii Kopiton <dd150285kvn@gmail.com>
2021-11-16 10:36:21 +0200

    hw7

--- src/com/pb/kopiton/hw7/Atelier.java
+++ src/com/pb/kopiton/hw7/Atelier.java
@@ -2,6 +2,13 @@ package com.pb.kopiton.hw7;
 
 public class Atelier {
     public static void main(String[] args) {
+        public static dressMan(Clothes[] clothes) {
+
+        }
+
+        static dressWomen(Clothes[] clothes) {
+
+        }
 
     }
 }
